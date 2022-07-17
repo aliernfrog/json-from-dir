4d089df7e53a441b8a0f89556bebd413431609b0
uafl00iZeT8k917 <45766489+aliernfrog@users.noreply.github.com>
2022-07-17 14:22:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const core = require("@actions/core");
+
+const folder = core.getInput("folder");
+const output = core.getInput("output");
+
+async function main() {
+  try {
+    const files = fs.readdirSync(folder).filter(file => file.endsWith(".json"));
+    const jsonData = [];
+    files.forEach(file => {
+      const content = fs.readFileSync(`${folder}/${file}`, "utf8");
+      jsonData.push(JSON.parse(content));
+    });
+    fs.writeFileSync(output, JSON.stringify(jsonData, null, 1));
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+}
